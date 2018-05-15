76c0dc6d177fd6e2b645c1bbe7660c56f68d0245
orionwambert <wambertorion@gmail.com>
2018-05-15 12:00:47 +0100

    Second Commit

--- about.php
+++ about.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Learning Adobe PhoneGap</title>
+	<title>Learning PhoneGap</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<h1>Hello World</h1>
 
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+<h1>About Page</h1>
+
+<p><em>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-	<a href="about.php">click me</a>
-
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</em></p>
 </body>
 </html>
